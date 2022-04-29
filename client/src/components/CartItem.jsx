068ded5f4bf02ed0fcbca8183fd534d2faca8f64
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-29 10:14:25 -0400

    remove item and dec qty

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 import useCart from '../hooks/useCart'
 
 const CartItem = ({ item }) => {
-  const { editQty } = useCart()
+  const { editQty, removeItem } = useCart()
+
+  const handleDecrement = async (item) => {
+    if (item.qty === 1) {
+      removeItem(item)
+    } else {
+      editQty(item.id, --item.qty)
+    }
+  }
 
   return (
     <div>
       <h6>{item.name}</h6>
       <h6>Qty: {item.qty}</h6>
       <button onClick={() => editQty(item.id, ++item.qty)}>+</button>
-      <button onClick={() => editQty(item.id, --item.qty)}>-</button>
+      <button onClick={() => handleDecrement(item)}>-</button>
+      <button onClick={() => removeItem(item)}>Remove from Cart</button>
     </div>
   )
 }
--- client/src/components/SingleProduct.jsx
+++ client/src/components/SingleProduct.jsx
@@ -3,15 +3,22 @@ import React from 'react'
 import useCart from '../hooks/useCart'
 
 const SingleProduct = ({ product }) => {
-  const { addItem, removeItem } = useCart()
+  const { addItem, cart } = useCart()
 
   return (
     <div>
       <h3>{product.name}</h3>
       <h4>{product.description}</h4>
       <h4>{product.price}</h4>
-      <button onClick={() => addItem(product)}>Add to Cart</button>
-      <button onClick={() => removeItem(product)}>Remove from Cart</button>
+      {cart.items.filter((item) => {
+        if (item.name === product.name) {
+          return item
+        }
+      }).length ? (
+        <h5>Already in Cart!</h5>
+      ) : (
+        <button onClick={() => addItem(product)}>Add to Cart</button>
+      )}
     </div>
   )
 }
